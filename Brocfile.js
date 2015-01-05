423b69ed74b319dc17b93f23e5d6832a6a9a356f
Kelly Selden <kellyselden@gmail.com>
2015-01-04 23:21:36 -0500

    app import

--- Brocfile.js
+++ Brocfile.js
@@ -18,6 +18,4 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/ember-animate/ember-animate.js');
-
 module.exports = app.toTree();
--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-slide-animation'
+  name: 'ember-cli-slide-animation',
+
+  included: function(app) {
+    app.import(app.bowerDirectory + '/ember-animate/ember-animate.js');
+  }
 };
